962d81e324c57e6bc9e5cd8fa83f8e204a13b1bc
Gil Domingues <gmdz95@gmail.com>
2015-12-21 20:12:56 +0000

    Pieces are distributed correctly between the two players

--- dominup/PrologServer.js
+++ dominup/PrologServer.js
@@ -1,34 +1,37 @@
-function PrologServer(scene, port){
-  this.requestPort = port || 8801;
+server = null;
+function PrologServer(scene){
   this.scene = scene;
+  server = this;
 }
 
-PrologServer.prototype.getPrologRequest = function(requestString, onSuccess, onError){
-	var request = new XMLHttpRequest();
-	request.open('GET', 'http://localhost:'+this.requestPort+'/'+requestString, true);
+PrologServer.prototype.getPrologRequest = function(requestString, onSuccess, onError, port){
+  var requestPort = port || 8081
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:'+requestPort+'/'+requestString, true);
 
-	request.onload = handleReply || function(data){console.log("Request successful. Reply: " + data.target.response);};
-	request.onerror = onError || function(){console.log("Error waiting for response");};
+  request.onload = this.handleReply || function(data){console.log("Request successful. Reply: " + data.target.response);};
+  request.onerror = onError || function(){console.log("Error waiting for response");};
 
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send();
-}
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  request.send();
+};
 
-
-//Handle the Reply
-function handleReply(data){
-	if(data != null && data != "ok")
-    parseStartGame(data);
-	return data.target.response;
-}
-
-function parseStartGame(argArray){
+PrologServer.prototype.parseStartGame = function(argArray){
   var argumentsArray = JSON.parse(argArray);
 
   var table = argumentsArray[0];
   var dominoes1 = argumentsArray[1];
   var dominoes2 = argumentsArray[2];
 
-  this.scene.players['player1'].setPieces(dominoes1);
-  this.scene.players['player2'].setPieces(dominoes2);
-}
+  server.scene.players['player1'].setPieces(dominoes1);
+  server.scene.players['player2'].setPieces(dominoes2);
+};
+
+
+//Handle the Reply
+PrologServer.prototype.handleReply = function(data){
+	if(data.target.response != null && data.target.response != "ok"){
+    server.parseStartGame(data.target.response);
+  }
+	return data.target.response;
+};
